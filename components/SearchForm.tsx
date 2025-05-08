f7f12f0cc69a17020e907f82cac9adbda493d550
dev <almas1610@gmail.com>
2025-05-09 03:13:14 +0500

    [feature]: Added SearchForm component with s-params & some cleaning code

---
+++ components/SearchForm.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Form from "next/form";
+import SearchFormReset from "@/components/SearchFormReset";
+
+const SearchForm = ({query}: {query?: string}) => {
+
+    return (
+        <Form action={'/'} scroll={false} className="search-form">
+            <input
+                name="query"
+                defaultValue={query}
+                className="search-input"
+                placeholder="Search..."
+            />
+
+            <div className="flex gap-2">
+                {query && <SearchFormReset />}
+
+                <button type="submit" className="search-btn text-white">
+                    🔎
+                </button>
+            </div>
+        </Form>
+    )
+}
+
+export default SearchForm;
