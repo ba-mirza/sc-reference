b20397719a897fd281076548f5b59b2acc436791
dev <almas1610@gmail.com>
2025-05-09 02:40:41 +0500

    [deps&code]: Shadcn configuration & layout section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
       <>
-        <h1 className="heading">Home</h1>
+          <section className="pink_container">
+              <h1 className="heading">Pitch your startup,
+                  <br/> Connect with Entrepreneurs</h1>
+              <p className="sub-heading !max-w-3xl">
+                  Submit Ideas, Vote on Pitches, and Get Noticed in virtual Competitions.
+              </p>
+          </section>
       </>
   );
 }
